96c0fad3bfa49ac3c17482bbbaec4a6dc8ad50bf
SeedPaths-User <lessdismissive@email.com>
2014-05-06 17:00:45 -0600

    v 1.0

---
+++ MyBlog/MyBlog/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Step 1. Set up database access
+        Models.myBlogEntities db = new Models.myBlogEntities();
+
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var list = db.Posts.OrderByDescending(x => x.PostedOn);
+            return View(list);
+        }
+
+    }
+}
---
+++ MyBlog/MyBlog/Models/PostValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyBlog.Models
+{
+    [MetadataType(typeof(PostMetaData))]
+    public partial class Post
+    {
+    }
+
+    public class PostMetaData
+    {
+        [Required(ErrorMessage="You need to enter a title")
+        , Display(Name="Title")]
+        public string PostTitle;
+
+        [Required(ErrorMessage = "Author name required")
+        , Display(Name="Author")]
+        public string PostAuthor;
+
+        [Required(ErrorMessage = "A Date is required")
+        , Display(Name = "Date")]
+        public string PostedOn;
+
+        [Display(Name="Picture (Optional)")]
+        public string PostPic;
+
+        [Required(ErrorMessage="Text body is required")
+        , Display(Name="Body")]
+        public string PostBody;
+
+        [Display(Name="Tags (Optional)")]
+        public string PostTags;
+    }
+}
