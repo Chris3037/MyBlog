474537b2a11556451e9eceb5479e35e0325377d7
Chris3037 <lessdismissive@email.com>
2014-05-09 17:00:04 -0600

    V 5.0

---
+++ MyBlog/MyBlog/Scripts/j.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#sideBar').on('mouseenter', function () {
+        $(this).animate({ 'left': '0px' }, 'fast');
+    });
+    $('#sideBar').on('mouseleave', function () {
+        $(this).animate({ 'left': '-100px' }, 'fast');
+    });
+});
